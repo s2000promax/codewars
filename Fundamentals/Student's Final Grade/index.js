165ec003ab9d04c0d5cca7ec6ac9e2c1b70cfa07
Stan <s2000ProMax@gmail.com>
2022-01-20 16:28:37 +0300

    update

---
+++ Fundamentals/Student's Final Grade/index.js
@@ -0,0 +1,8 @@
+function finalGrade (exam, projects) {
+  return exam > 90 || projects > 10 ? 100
+    : exam > 75 && projects >=5 ? 90
+      : exam > 50 && projects >=2 ? 75
+        : 0
+}
+
+console.log(finalGrade(0, 10));

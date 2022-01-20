a3c5943fdd5b19e459f79b87a7cd9b952f03f563
Stan <s2000ProMax@gmail.com>
2022-01-20 16:34:14 +0300

    update

---
+++ Fundamentals/Switch it Up/index.js
@@ -0,0 +1,10 @@
+function switchItUp(number){
+  const dictionary = ['Zero', 'One', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine'];
+
+  return dictionary[number];
+}
+
+
+console.log(switchItUp(0));
+console.log(switchItUp(1));
+console.log(switchItUp(2));
